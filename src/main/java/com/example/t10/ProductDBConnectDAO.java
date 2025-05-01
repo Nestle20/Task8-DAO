23e4c75e6c0fef833464d4965875c4316610d2a0
Nestle20 <nastyaserdyukova3@gmail.com>
2025-05-01 23:42:31 +0300

    Сделали преобразования в программе

--- src/main/java/com/example/t10/ProductDBConnectDAO.java
+++ src/main/java/com/example/t10/ProductDBConnectDAO.java
@@ -1,7 +1,10 @@
 package com.example.t10;
 
 import javax.sql.rowset.CachedRowSet;
-import java.sql.*;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
